65aa725c487ec95ab8e7ed06ff3c35eae8de482f
casey tokarchuk <caseytokarchuk@gmail.com>
2022-08-16 14:32:13 -0400

    added user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,23 @@
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+emails = %w[hugo@toilet.com casey@toilet.com ege@toilet.com Adou@toilet.com Kieren@toilet.com stephane@toilet.com test@gmail.com]
 
+# Seed users
+puts "Destroying users..."
 User.destroy_all
-user = User.create!(email: "test@test.com", first_name: "Casey", last_name: "Tokarchuk", phone_number: "123456789", password: "password")
+counter = 0
+7.times do
+    User.create!(email: emails[counter],
+    first_name: Faker::TvShows::Seinfeld.character,
+    last_name: Faker::Name.last_name ,
+    phone_number: Faker::PhoneNumber.phone_number,
+    password: Faker::Lorem.characters(number: 8))
+    counter += 1
+    puts "Created user #{counter + 1}"
+end
+puts "Finished generating users!"
 
+# Seed bathrooms
 puts "Cleaning your bathrooms..."
 Bathroom.destroy_all
-
 puts "Building bathrooms..."
 
 counter = 1
@@ -14,8 +25,8 @@ counter = 1
   Bathroom.create!(price: rand(0..100),
   location: Faker::Nation.capital_city,
   description: Faker::TvShows::Seinfeld.quote,
-  user: user)
+  user: User.order('RANDOM()').first)
   puts "Created Bathroom #{counter}"
   counter += 1
 end
-puts "Finsihed!"
+puts "Finished!"
