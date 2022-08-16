0304c11ef14aad0c6274625c84bf3398f5d68ef8
Ege Hidiroglu <egehdrgl@gmail.com>
2022-08-16 11:48:48 -0400

    Trying to push

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # Validations
   validates :email, presence: true, format: { with: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/ }
 end
