6026c10e3afc1849bb53d262e24c81983e6492e5
Ege Hidiroglu <egehdrgl@gmail.com>
2022-08-18 11:22:16 -0400

    Added geocode gem, configured geocoder.rb file, add longitude and latitude to bathrooms through migration

--- Gemfile
+++ Gemfile
@@ -83,3 +83,5 @@ group :test do
 end
 
 gem 'cloudinary'
+
+gem 'geocoder'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_16_200017) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_18_152043) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_16_200017) do
     t.datetime "updated_at", null: false
     t.integer "availability", default: 1
     t.text "description"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_bathrooms_on_user_id"
   end
 
