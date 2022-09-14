2edb71593bd71aa1f5a53947916b23e895413266
casey tokarchuk <caseytokarchuk@gmail.com>
2022-09-13 22:42:38 -0400

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,55 +6,55 @@ bathrooms = %w[https://www.thespruce.com/thmb/qkmtQZFjNVUysd60GF49V7hag0A=/2048x
   https://usagainstthehouse.files.wordpress.com/2018/08/img_1985-1.jpg?w=1100
   https://cdn.mos.cms.futurecdn.net/urnQmjBrp6U2nnAqzzf2gb.jpg
   https://www.mydomaine.com/thmb/ak1nhpTFDwZI1WnEly6uv-drubI=/4096x2734/filters:no_upscale():max_bytes(150000):strip_icc()/unnamed-2-cfd97c57375745ceaa0bfa89c07d1e8b.jpg
-  https://cdn.vox-cdn.com/thumbor/vpKcH3hmABka8IHeWNheA7UFiGs=/0x0:3600x2400/1200x800/filters:focal(1512x912:2088x1488)/cdn.vox-cdn.com/uploads/chorus_image/image/71160072/Web_ConcordCape_01102022JK_0373.0.jpg
+  https://i.insider.com/5ea885b4e61f344041672da9?width=700
 ]
 users = %w[https://assets.teenvogue.com/photos/62485a6e6f883de6edfb2e9b/1:1/w_2995,h_2995,c_limit/GettyImages-1340003502.jpeg
-  https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTE1ODA0OTcxNjIzMzUxODIx/queen-elizabeth-ii-9286165-1-402.jpg
-  https://www.biography.com/.image/t_share/MTc5ODc5NjY5ODU0NjQzMzIy/gettyimages-3091504.jpg
+  https://upload.wikimedia.org/wikipedia/en/thumb/d/de/Barney_Gumble.png/220px-Barney_Gumble.png
+  https://upload.wikimedia.org/wikipedia/en/3/33/Elaine-benes-3707.jpg
   https://www.biography.com/.image/t_share/MTgxMDEyMDQ4NzE5NjUxOTI4/gettyimages-471835002.jpg
-  https://www.biography.com/.image/t_share/MTgxMDEyMDQ4NzE5NjUxOTI4/gettyimages-471835002.jpg
+  https://static.wikia.nocookie.net/p__/images/1/17/Jeff_daniels.jpg/revision/latest?cb=20130118205053&path-prefix=protagonist
   https://cdn.vox-cdn.com/thumbor/nyfeTpnaEB6BFBhFlG1k7dvWBFE=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/20077872/star_jeffree.jpg
   https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTY3MDUxMjkzMjI1OTIwMTcz/brad-pitt-attends-the-premiere-of-20th-century-foxs--square.jpg
-  https://www.instyle.com/thmb/_-x4lueIVPGZi4b8Q5-4g52TooI=/1521x1521/smart/filters:no_upscale():focal(713x555:715x557)/Jessica-Alba-Lead-9d945b2f645a439aaf6563703122348b.jpg
+  https://upload.wikimedia.org/wikipedia/en/thumb/d/dc/MichaelScott.png/220px-MichaelScott.png
 ]
 emails = %w[megan@fox.com
-  queen@elizabeth.com
-  albert@einstein.com
+  barney@gumble.com
+  elaine@bennis.com
   hussain@bolt.com
-  jeffree@star.com
+  harry@dunn.com
   brad@pitt.com
   jessica@alba.com
-  edward@cullen.com
+  michael@dundermifflin.com
   ]
 
 first_names = %w[Megan
-  Queen
-  Albert
+  Barney
+  Elaine
   Hussain
-  Jeffree
+  Harry
   Brad
   Jessica
-  Edward
+  Michael
   ]
 
   last_names = %w[Fox
-    Elizabeth
-    Einstein
+    Gumble
+    Bennis
     Hussain
-    Star
+    Dunn
     Pitt
     Alba
-    Cullen
+    Scott
     ]
 
 descriptions = ["Enjoy an amazing experince in my bathroom with me and M.G.Kelly",
-"Buckingham Palace's doors are open to you",
-"Want to hear about relativity when you're relieving yourself?",
+"I haven't had a job in six years, hoping this will pay the bills!",
+"I can spare a square, if you need",
 "Can you beat my record?",
-"I'll do your make up too",
-"Angeline made this bathroom for me and our 36 kids",
-"Julien's biggest dream!",
-"I will stare at you deeply when you do your thing...",
+"Very well equipped washroom, you will have no issues",
+"Luxurious bathroom, take your time!",
+"Cleanest bathroom in Montreal!",
+"People say I am the best boss and have the best bathroom",
 ]
 
 
@@ -63,7 +63,7 @@ address = ["5589 Rue Clark, Montreal",
   "5286 St Andre St, Montreal",
   "1108 Rue Gilford, Montreal",
   "800 Duluth Ave E, Montreal",
-  "93 Maguire St, Montreal",
+  "4405 Rue Marquette, Montreal",
   "5371 Rue Clark, Montreal",
   "5417 Rue Saint-Urbain, Montreal",
 ]
@@ -90,7 +90,7 @@ users.each_with_index do |value, index|
 
     puts "Creating bathrooms..."
     bathroom = Bathroom.new(
-      price: rand(50..100),
+      price: rand(3..15),
       address: address[counter],
       description: descriptions[counter]
   )
